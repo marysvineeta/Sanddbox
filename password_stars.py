6c0a17f818b8e351226a5790006b5ade32326c8d
marys003 <marysvineeta.paul@my.jcu.edu.au>
2024-06-12 10:16:40 +1000

    Add password check program

---
+++ password_stars.py
@@ -0,0 +1,12 @@
+min_length = 8
+
+while True:
+
+    password = input("Enter a password: ")
+
+    if len(password) >= min_length:
+        break
+    else:
+        print(f"Password must be at least {min_length} characters long. Please try again.")
+
+print('*' * len(password))
